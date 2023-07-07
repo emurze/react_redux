00213a7df5c850d328569582867bdcbda6d1bb25
Fat Bunny <rozanov7475@yandex.by>
2023-07-07 18:31:52 +0300

    add new react-redux hooks

--- src/components/Menu/AddItemForm/AddItemForm.js
+++ src/components/Menu/AddItemForm/AddItemForm.js
@@ -1,47 +1,37 @@
 import './AddItemForm.css';
 import React from 'react';
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {
 	AddItem,
 	UpdatePreparedAddItem,
 } from "../../../redux/ActionCreators";
 
-const AddItemForm = (props) => {
+const AddItemForm = () => {
+	let {prepared_id} = useSelector(
+		state => state.AppData.MenuData.AddItemData
+	)
+	let dispatch = useDispatch()
+	
+	const add_item = () => {
+		const addItemAction = AddItem()
+		dispatch(addItemAction)
+	}
+	
 	const update_item = (elem) => {
 		const id = elem.target.value
-		return props.UpdatePreparedItem(id)
+		const updateItemAction = UpdatePreparedAddItem(id)
+		dispatch(updateItemAction)
 	}
 	return (
 		<div className="add_item_form">
 			<p><input type='text'
 					  onChange={ update_item }
-					  value={ props.prepared_id }
+					  value={ prepared_id }
 					  placeholder='id'/></p>
 			
-			<p><button onClick={ props.AddItem }>Add item</button></p>
+			<p><button onClick={ add_item }>Add item</button></p>
 		</div>
 	)
 }
 
-const mapStateToProps = (state) => {
-	const AddItemData = state.AppData.MenuData.AddItemData
-	return {
-		'prepared_id': AddItemData.prepared_id,
-		'prepared_title': AddItemData.prepared_title
-	}
-}
-
-const mapDispatchToProps = (dispatch) => {
-	return {
-		'AddItem': () => {
-			const action = AddItem()
-			return dispatch(action)
-		},
-		'UpdatePreparedItem': (id) => {
-			const action = UpdatePreparedAddItem(id)
-			return dispatch(action)
-		}
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddItemForm);
+export default AddItemForm;
--- src/components/Menu/PopItemForm/PopItemForm.js
+++ src/components/Menu/PopItemForm/PopItemForm.js
@@ -1,39 +1,35 @@
 import './PopItemForm.css';
 import React from "react";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {PopItem, UpdatePreparedPopItem} from "../../../redux/ActionCreators";
 
-const PopItemForm = (props) => {
+const PopItemForm = () => {
+	let {prepared_id} = useSelector(
+		state => state.AppData.MenuData.PopItemData
+	)
+	let dispatch = useDispatch()
+	
+	const pop_item = () => {
+		const pop_action = PopItem()
+		dispatch(pop_action)
+	}
 	const update_item = (elem) => {
 		const id = elem.target.value
-		props.update_prepared_item(id)
+		const update_action = UpdatePreparedPopItem(id)
+		return dispatch(update_action)
 	}
 	
 	return (
 		<div className="pop_item_form">
 			<p><input type='text'
 					  onChange={ update_item }
-					  value={ props.prepared_id }
+					  value={ prepared_id }
 					  placeholder='id' /></p>
 			
-			<p><button onClick={ props.pop_item }>Pop item</button></p>
+			<p><button onClick={ pop_item }>Pop item</button></p>
 		</div>
 	)
 }
 
-const mapStateToProps = (state) => ({
-	'prepared_id': state.AppData.MenuData.PopItemData.prepared_id
-})
 
-const mapDispatchToProps = (dispatch) => ({
-	'pop_item': () => {
-		const pop_action = PopItem()
-		return dispatch(pop_action)
-	},
-	'update_prepared_item': (id) => {
-		const update_action = UpdatePreparedPopItem(id)
-		return dispatch(update_action)
-	}
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(PopItemForm);
+export default PopItemForm;
